b22b0f0748e29896603d10fe870816e5724107c2
tegrakmd <tegrakamanda@gmail.com>
2026-07-10 19:35:01 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,134 @@
+gsap.registerPlugin(CustomEase, SplitText);
+
+CustomEase.create("hop", "0.8,0,0.2,1");
+CustomEase.create("hop2", "0.9,0,0.1,1");
+
+const splitText = (selector, type, className, mask = true) => {
+  return SplitText.create(selector, {
+    type: type,
+    [`${type}Class`]: className,
+    ...(mask && { mask: type }),
+  });
+};
+const preloaderHeaderSplit = splitText(".preloader-header h1", "chars", "char");
+const navSplit = splitText("nav a", "words", "word");
+const headerSplit = splitText(".header h1", "chars", "char", false);
+const footerSplit = splitText(".hero-footer p", "words", "word");
+
+const preloaderImgInitRotations = [7.5, -2.5, -10, 12.5, -5, 5];
+gsap.set(".preloader-image", {
+  rotate: (i) => preloaderImgInitRotations[i],
+});
+
+const tl = gsap.timeline({ delay: 0.5 });
+
+tl.to(".preloader-image", {
+  scale: 1,
+  clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+  duration: 1,
+  ease: "hop",
+  stagger: 0.2,
+});
+tl.to(
+  ".preloader-header h1 .char",
+  {
+    y: "0%",
+    duration: 1,
+    ease: "hop2",
+    stagger: { each: 0.125, from: "random" },
+  },
+  0.35
+);
+tl.to(
+  ".preloader-counter p",
+  {
+    y: "0%",
+    ease: "hop2",
+    onStart: () => {
+      const counterEl = document.querySelector(".preloader-counter p");
+      const counter = { value: 0 };
+      gsap.to(counter, {
+        value: 100,
+        duration: 2,
+        delay: 0.5,
+        ease: "power1.inOut",
+        onUpdate: () => {
+          counterEl.textContent = String(Math.round(counter.value)).padStart(
+            3,
+            "0"
+          );
+        },
+      });
+    },
+  },
+  "<"
+);
+tl.to(
+  ".preloader-counter p",
+  {
+    y: "-100%",
+    duration: 0.75,
+    ease: "hop2",
+  },
+  3.25
+);
+tl.to(
+  ".preloader-header h1 .char",
+  {
+    y: "-100%",
+    duration: 0.75,
+    ease: "hop2",
+    stagger: { each: 0.125, from: "random" },
+  },
+  3.25
+);
+tl.to(
+  ".preloader-images .preloader-image",
+  {
+    scale: 0,
+    clipPath: "polygon(20% 20%, 80% 20%, 80% 80%, 20% 80%)",
+    duration: 1,
+    ease: "hop2",
+    stagger: -0.075,
+  },
+  3.5
+);
+tl.to(
+  ".preloader",
+  {
+    clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+    duration: 1,
+    ease: "hop2",
+  },
+  4.35
+);
+tl.to(
+  ".header h1 .char",
+  {
+    y: "0%",
+    duration: 1,
+    ease: "hop",
+    stagger: { each: 0.125, from: "random" },
+  },
+  4.35
+);
+tl.to(
+  "nav a .word",
+  {
+    y: "0%",
+    duration: 1,
+    ease: "hop",
+    stagger: 0.075,
+  },
+  4.75
+);
+tl.to(
+  ".hero-footer p .word",
+  {
+    y: "0%",
+    duration: 1,
+    ease: "hop",
+    stagger: 0.075,
+  },
+  4.75
+);
